4f7528c0ab1685f6be5926dbf9ad2fb3fcacdd79
Jason McDermott <jmcdermott@rightpoint.com>
2018-11-03 10:48:56 -0500

    module 2

--- src/app.js
+++ src/app.js
@@ -10,6 +10,36 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.render('index', { title: 'Index' }));
+const accountData = fs.readFileSync(
+    path.join(__dirname, 'json', 'accounts.json'), 'utf8'
+);
+const accounts = JSON.parse(accountData);
 
-app.listen(3000, () => console.log('banking portal running on port 3000'));
+const userData = fs.readFileSync(
+    path.join(__dirname, 'json', 'users.json'), 'utf8'
+);
+const users = JSON.parse(userData);
+
+app.get('/', (req, res) => { 
+    res.render('index', { title: 'Account Summary', accounts })
+});
+
+app.get('/savings', (req, res) => {
+    res.render('account', { account: accounts.savings })
+});
+
+app.get('/checking', (req, res) => {
+    res.render('account', { account: accounts.checking })
+});
+
+app.get('/credit', (req, res) => {
+    res.render('account', { account: accounts.credit })
+});
+
+app.get('/profile', (req, res) => {
+    res.render('profile', { user: users[0] })
+});
+
+app.listen(3000, () => { 
+    console.log('banking portal running on port 3000')
+});
